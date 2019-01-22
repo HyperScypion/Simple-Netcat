209da94529343ed341b2dea3001d571c39e5644c
hyperscypion <tomasz_derek@wp.pl>
2019-01-22 18:09:00 +0100

    Fixed server.h problem

--- server.h
+++ server.h
@@ -1,5 +1,11 @@
-#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-int server()
+int server_tcp(unsigned short int port);
